13a6b3cc2d2b2bd789371bd2c6a855416d5dfbda
Muhammad-irfan12345 <irfandhudi.vu@gmail.com>
2022-04-03 23:03:50 +0500

    FYP Front-End

--- resources/views/layouts/footers/auth.blade.php
+++ resources/views/layouts/footers/auth.blade.php
@@ -1,11 +1,11 @@
 <footer class="footer">
   <div class="container-fluid">
-    <div class="copyright float-right">
+    {{-- <div class="copyright float-right">
       &copy;
       <script>
         document.write(new Date().getFullYear())
       </script>, made with <i class="material-icons">favorite</i> by
-      <a href="https://www.laraartisan.com" target="_blank">Lara Artisan</a>
-    </div>
+      <a href="https://www.laraartisan.com" target="_blank"></a>
+    </div> --}}
   </div>
 </footer>
